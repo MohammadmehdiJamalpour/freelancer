d81f0f3188a0c456d5e71850174d2f99727826e8
mohammadmehdijamalpour <mohammadmehdi.jamalpour28@gmail.com>
2026-02-11 19:38:11 +0330

    authentication feature added + bug in using custom colors got fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Auth from "./pages/Auth";
 import { Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from './../node_modules/react-hot-toast/src/components/toaster';
+import "./index.css";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <Routes>
-      <Route path="/auth" element={<Auth />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Toaster/>
+      <div className="container xl:max-w-Screen-xl">
+        <Routes>
+          <Route path="/auth" element={<Auth/>} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+     
+        <App />
     </React.StrictMode>
   </BrowserRouter>,
 );
